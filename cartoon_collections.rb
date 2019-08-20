38f81419ee87ac26790a5c50dbb770e9880ae681
Donovan Jones <donovanljones94@gmail.com>
2019-08-20 19:58:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(array)
-  array.collect(array.capitalize)
+  array.collect {|x| x.capitalize}
 end
 
 def long_planeteer_calls# code an argument here
